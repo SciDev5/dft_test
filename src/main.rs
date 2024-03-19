c2c6cad11c118965288a908244ddf2fb8376bbf2
SciDev5 <skhuber0@gmail.com>
2024-03-18 22:50:24 -0400

    turns out you can just tell it not to crash (increased stack size)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 use dft::{dft, gen_v_ion};
 use num_complex::Complex64;
 use vol::{Volume, VolumeBasisSet};
@@ -9,15 +11,24 @@ pub mod linear;
 pub mod vol;
 
 fn main() {
-    let dr = 1.0;
-    let basis: VolumeBasisSet<Complex64, 4, 4, 4> = VolumeBasisSet::plane_waves([2, 2, 2]);
-    let v_ion = gen_v_ion(
-        &[([2.0, 2.0, 2.0], IonData::Simple { z: 1 })],
-        0,
-        dr,
-        &basis,
-    );
-    let mut n = Volume::new_from_total_density(1.0);
-    let (_phi_0, spectrum) = dft(&mut n, &basis, &v_ion, dr, 100);
-    dbg!(n, spectrum);
+    thread::Builder::new()
+        .stack_size(1 << 30)
+        .name(format!("cool thread"))
+        .spawn(|| {
+            let dr = 1.0;
+            let basis: VolumeBasisSet<Complex64, 10, 10, 10> =
+                VolumeBasisSet::plane_waves([5, 5, 5]);
+            let v_ion = gen_v_ion(
+                &[([2.0, 2.0, 2.0], IonData::Simple { z: 1 })],
+                0,
+                dr,
+                &basis,
+            );
+            let mut n = Volume::new_from_total_density(1.0);
+            let (_phi_0, spectrum) = dft(&mut n, &basis, &v_ion, dr, 100);
+            dbg!(n, spectrum);
+        })
+        .unwrap()
+        .join()
+        .unwrap();
 }
